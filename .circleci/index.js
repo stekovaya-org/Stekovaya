3eea64cf049de1d8770ddf2cc9dbeeff06c16067
Stekovaya <68231194+Stekovaya@users.noreply.github.com>
2020-08-01 07:45:13 +0900

    Create index.js

---
+++ .circleci/index.js
@@ -0,0 +1,18 @@
+var io = require("socket.io-client")("https://server.stekovaya.repl.co");
+var fs = require("fs");
+io.on("connect",()=>{
+  fs.readdirSync("sample/console").forEach(x=>{
+    if(x == "likerogue.stk" || x == "montecarlo.stk" || x == "pi.stk" || x == "fibonacci.stk") return;
+    console.log("Running " + x + "...");
+    io.emit("run",fs.readFileSync("sample/console/" + x) + "",d=>{
+      if(d.s == 0){
+        console.log("success");
+      }else{
+        console.log("fail");
+        console.log(d.r);
+        process.exit(1);
+      }
+      console.log(d.r);
+    });
+  });
+});
