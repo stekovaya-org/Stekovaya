014842a414a07d939d592f395197066abe2c7991
Stekovaya <68231194+Stekovaya@users.noreply.github.com>
2020-08-01 08:03:09 +0900

    Update index.js

--- .circleci/index.js
+++ .circleci/index.js
@@ -2,7 +2,7 @@ var io = require("socket.io-client")("https://server.stekovaya.repl.co");
 var fs = require("fs");
 io.on("connect",()=>{
   fs.readdirSync("sample/console").forEach(x=>{
-    if(x == "likerogue.stk" || x == "montecarlo.stk" || x == "pi.stk" || x == "fibonacci.stk") return;
+    if(x == "likerogue.stk" || x == "montecarlo.stk" || x == "pi.stk" || x == "fibonacci.stk" || x == "yourname.stk") return;
     console.log("Running " + x + "...");
     io.emit("run",fs.readFileSync("sample/console/" + x) + "",d=>{
       if(d.s == 0){
