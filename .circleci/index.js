119c9a9472c967c02de881b543f8cf1d6c7d1adc
Stekovaya <68231194+Stekovaya@users.noreply.github.com>
2020-08-01 08:16:25 +0900

    Update index.js

--- .circleci/index.js
+++ .circleci/index.js
@@ -1,7 +1,9 @@
 var io = require("socket.io-client")("https://server.stekovaya.repl.co");
 var fs = require("fs");
 io.on("connect",()=>{
-  fs.readdirSync("sample/console").forEach((x,i)=>{
+  var i = 0;
+  fs.readdirSync("sample/console").forEach(x=>{
+    i++;
     if(x == "likerogue.stk" || x == "montecarlo.stk" || x == "pi.stk" || x == "fibonacci.stk" || x == "yourname.stk") return;
     console.log("Running " + x + "...");
     io.emit("run",fs.readFileSync("sample/console/" + x) + "",d=>{
@@ -13,7 +15,7 @@ io.on("connect",()=>{
         process.exit(1);
       }
       console.log(d.r);
-      if(i - 1 == fs.readdirSync("sample/console").length) process.exit(0);
+      if(i == fs.readdirSync("sample/console").length) process.exit(0);
     });
   });
 });
