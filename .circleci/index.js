2ad6cd3cbf88439b43c005db5de87bb2cc9924e9
Stekovaya <68231194+Stekovaya@users.noreply.github.com>
2020-08-06 14:59:33 +0900

    Update index.js

--- .circleci/index.js
+++ .circleci/index.js
@@ -1,37 +1,40 @@
-var io = require("socket.io-client")("https://server.stekovaya.repl.co");
 var fs = require("fs");
 var i = -1;
 var dl = JSON.parse(fs.readFileSync(".circleci/dening.json") + "");
-io.on("connect",()=>{
-  var ds = fs.readdirSync("sample/console");
-  function fc(){
-    var x = ds[++i];
-    process.stdout.write("Running " + x + "...   ");
-    if(dl.includes(x)){
-      console.log("denied");
-      if(i + 1 == ds.length){
-        process.exit(0);
-      }else{
-        fc();
-        return;
-      }
+var ds = fs.readdirSync("sample/console");
+function fc(){
+  var x = ds[++i];
+  process.stdout.write("Running " + x + "...   ");
+  if(dl.includes(x)){
+    console.log("denied");
+    if(i + 1 == ds.length){
+      process.exit(0);
+    }else{
+      fc();
+      return;
     }
-    console.log("running");
-    io.emit("run-stekovaya",fs.readFileSync("sample/console/" + x) + "",d=>{
-      if(d.s == 1){
-        console.log("-----fail-----");
-        console.log(d.r);
-        process.exit(1);
-      }else{
-        console.log("----success---");
-      }
-      console.log(d.r);
-      if(i + 1 == ds.length){
-        process.exit(0);
-      }else{
-        fc();
-      }
-    });
   }
-  fc();
-});
+  console.log("running");
+  var d = {};
+  try{
+    d.r = require("child_process").execSync("mono stkvy.exe sample/console/" + x + " 2>&1") + "";
+    d.s = 0;
+  }catch(err){
+    d.r = err.stdout + "";
+    d.s = 1;
+  }
+  if(d.s == 1){
+    console.log("-----fail-----");
+    console.log(d.r);
+    process.exit(1);
+  }else{
+    console.log("----success---");
+  }
+  console.log(d.r);
+  if(i + 1 == ds.length){
+    process.exit(0);
+  }else{
+    fc();
+  }
+}
+fc();
